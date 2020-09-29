f7fe1ff8664e023a18027c4a05281dd7732afcb5
joelsonlucass <46199455+joelsonlucass@users.noreply.github.com>
2020-09-28 22:48:13 -0300

    Initial commit

---
+++ confirmar_sair.php
@@ -0,0 +1,7 @@
+<script language="JavaScript">
+  window.onbeforeunload = confirmExit;
+  function confirmExit()
+  {
+    return "Se você fechar o navegador, seus dados serão perdidos. Desena Realmente sair?";
+  }
+</script>
